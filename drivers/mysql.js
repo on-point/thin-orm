8f13f79a133e53c75d19a28ead5c848d1e2c6d3e
Anatolij Ostroumov <ostroumov095@gmail.com>
2013-05-24 01:42:32 +0400

    initial - mysql driver

--- drivers/mysql.js
+++ drivers/mysql.js
@@ -1,17 +1,31 @@
 function createMySQLDriver(context, options) {
-    return {
+    return  {
+        query: function (query, parameters, id, callback) {
+            var self = context;
+            if (parameters instanceof Array) {
+                var parametersPrepared = parameters;
+            } else {
+                var parametersPrepared = [parameters];
+            }
+            var queryPrepared = query.replace(/\$\d/g, '\?');
+            options.db.query(queryPrepared, parametersPrepared, function (err, result) {
+                if (err)
+                    self.logger('query on ' + id + ':\n\ttext: ' + queryPrepared + JSON.stringify(parametersPrepared) + '\n\tfailed: ' + err);
+                else
+                    self.logger('query on ' + id + ':\n\ttext: ' + queryPrepared + '\n\tparams: ' + JSON.stringify(parametersPrepared) + '\n\treturns ' + JSON.stringify(result));
+                callback(err, result);
+            });
 
-        query: function testDriver(query, parameters, id, cb) {
-            options.data.query = query;
-            options.data.params = parameters;
-            cb(null, { count: 0, rows: [] });
         },
-
-        getInsertQueryText:function (table) {
+        // gets a SQL clause to have an INSERT query return the id of the new row
+        getInsertQueryText: function(table) {
             return "";
         },
 
-        getIdForInsert:function (table, result) {
+        // gets the id of a new row from the result of an INSERT query
+        getIdForInsert: function(table, result) {
+            if (result && result.insertId)
+                return result.insertId;
             return -1;
         }
     };
